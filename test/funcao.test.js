0fef7db68ec13755982edc45ee0c1554084e073c
Bryan Costa <bryan.costa@mercafacil.me>
2021-08-17 12:50:17 -0300

    update test

--- test/funcao.test.js
+++ test/funcao.test.js
@@ -1,6 +1,16 @@
-const mult = require ('./src/mult')
-
-test('5*5 deve ser 25', ()=>{
- expect(mult(5,5)).toBe(25)
-
+let Calc = require ('./src/mult')// faz a requisição da nossa classe
+describe('Calculadora', function(){// onde vamos fazer a bateria de teste
+    let calculadora = new Calc()
+it('deve multiplicar dois números', function(){
+    expect(calculadora.multiplicar(2,2)).toBe(4);  
+})
+it('deve subtrair dois números', function(){
+    expect(calculadora.subtrair(2,2)).toBe(0);
+})
+it('deve somar dois números', function(){
+    expect(calculadora.somar(2,2)).toBe(4);
+}) 
+it('deve dividir dois números', function(){
+    expect(calculadora.dividir(2,2)).toBe(1);
+}) 
 })
--- test/src/mult.js
+++ test/src/mult.js
@@ -1,2 +1,27 @@
-const mult = (a,b)=> a * b
-module.exports = mult;
+class Calc{
+    constructor(){
+        this.valorA = 0
+        this.valorB = 0
+    }
+    multiplicar(valA, valB){
+        this.valorA=valA;
+        this.valorB=valB;
+        return this.valorA * this.valorB;
+    }
+    somar(valA, valB){
+        this.valorA=valA;
+        this.valorB=valB;
+        return this.valorA + this.valorB;
+    }
+    dividir(valA, valB){
+        this.valorA=valA;
+        this.valorB=valB;
+        return this.valorA / this.valorB;
+    }
+    subtrair(valA, valB){
+        this.valorA=valA;
+        this.valorB=valB;
+        return this.valorA - this.valorB;
+    }
+}
+module.exports = Calc;
